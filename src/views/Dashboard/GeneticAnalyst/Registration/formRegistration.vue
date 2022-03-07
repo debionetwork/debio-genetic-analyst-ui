bd1efbacaca747498ce7ec9cbfb646a8e43c2b6b
luthfiazhari <luthfi@blocksphere.id>
2022-03-07 09:23:32 +0700

    Feat: GA registration page step 1

---
+++ src/views/Dashboard/GeneticAnalyst/Registration/formRegistration.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  .geneticAnalysts-registration-form
+    .geneticAnalysts-registration-form__card
+      .geneticAnalysts-registration-form__avatar-wrapper
+        .geneticAnalysts-registration-form__avatar-image
+          ui-debio-avatar(
+            :src="computedImageLink"
+            size="100"
+            rounded
+          )
+            ui-debio-file(
+              v-model="profileImage"
+              accept="[.jpg, .png, .jpeg]"
+
+            )
+
+
+</template>
+
+<script>
+export default {
+  name: "GeneticAnalystsForm",
+
+  data: () => ({
+    profileImage: null
+  }),
+
+  computed: {
+    computedImageLink() {
+      return this.profileImage ? this.profileImage : require("@/assets/add-image-placeholder.png")
+    }
+  },
+
+  methods: {
+    clickAvatar() {
+      console.log("clicked")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
