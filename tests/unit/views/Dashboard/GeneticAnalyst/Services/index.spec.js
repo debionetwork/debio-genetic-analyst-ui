1440ff795cfcafe2c7c2548b7f0f2fe53b4d7979
ciniiia <cinia@blocksphere.id>
2022-03-19 12:06:42 +0700

    tests: adjust unit test for GA services list page

--- tests/unit/views/Dashboard/GeneticAnalyst/Services/index.spec.js
+++ tests/unit/views/Dashboard/GeneticAnalyst/Services/index.spec.js
@@ -1,7 +1,16 @@
-import { createLocalVue, shallowMount } from "@vue/test-utils"
+import { createLocalVue, shallowMount, config } from "@vue/test-utils"
 import GAServices from "@/views/Dashboard/GeneticAnalyst/Services"
 import Vuex from "vuex"
 import Vuetify from "vuetify"
+
+config.stubs["ui-debio-error-dialog"] = { template: "<div></div>" }
+config.stubs["ui-debio-modal"] = { template: "<div></div>" }
+config.stubs["ui-debio-icon"] = { template: "<div></div>" }
+config.stubs["ui-debio-button"] = { template: "<div></div>" }
+config.stubs["ui-debio-banner"] = { template: "<div></div>" }
+config.stubs["ui-debio-card"] = { template: "<div></div>" }
+config.stubs["ui-debio-data-table"] = { template: "<div></div>" }
+
 
 describe("Genetic Analyst Services Dashboard", () => {
   let container
@@ -18,6 +27,13 @@ describe("Genetic Analyst Services Dashboard", () => {
   })
 
   it("Should render", () => {
+
+    GAServices.methods = {
+      getDeleteServiceFee: jest.fn(),
+      getServiceList: jest.fn(),
+      onDelete: jest.fn()
+    }
+
     container = shallowMount(GAServices, {
       localVue,
       vuetify: new Vuetify(),
@@ -27,9 +43,6 @@ describe("Genetic Analyst Services Dashboard", () => {
             api: "API",
             wallet: "WALLET",
             mnemonicData: "oil spend nation obey lecture behave lake diary reward forest gym apple"
-          },
-          auth: {
-            loadingData: "LOADING"
           },
           metamask: {
             web3: "WEB3"
