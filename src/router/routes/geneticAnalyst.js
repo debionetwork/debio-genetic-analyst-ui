89ba3aa163449946d7d477f2374a65bf0f8329bd
Azmi Muhammad <azmim85@gmail.com>
2022-05-24 15:22:18 +0700

    feat: GA regist

--- src/router/routes/geneticAnalyst.js
+++ src/router/routes/geneticAnalyst.js
@@ -1,4 +1,6 @@
-import { checkIsLoggedIn } from "@/common/lib/route-guard"
+import { checkIsLoggedIn, checkAccountStatus } from "@/common/lib/route-guard"
+import registrationRoutes from "./registration"
+import store from "@/store/index"
 
 export default [
   {
@@ -19,12 +21,23 @@ export default [
         path: "/",
         name: "ga-dashboard",
         meta: { pageHeader: "Home" },
-        component: () => import(/* webpackChunkName */ "@/views/Dashboard/GeneticAnalyst/Home")
+        component: () => import(/* webpackChunkName */ "@/views/Dashboard/GeneticAnalyst/Home"),
+        beforeEnter: checkAccountStatus
+      },
+      {
+        path: "/",
+        name: "ga-dashboard-verification",
+        meta: { pageHeader: "Home" },
+        component: () => import(/* webpackChunkName */ "@/views/Dashboard/GeneticAnalyst/Home/Unverified"),
+        beforeEnter: (to, from, next) => {
+          if (store.state.substrate.GAAccount.verificationStatus === "Verified") next({ name: "ga-dashboard" })
+          else next()
+        }
       },
       {
         path: "my-account",
         name: "ga-account",
-        meta: { pageHeader: "My Account"},
+        meta: { pageHeader: "My Account" },
         component: () => import(/* webpackChunkName */ "@/views/Dashboard/GeneticAnalyst/Account")
       },
       {
@@ -36,7 +49,7 @@ export default [
       {
         path: "orders",
         name: "ga-orders",
-        meta: { pageHeader: "Orders"},
+        meta: { pageHeader: "Orders" },
         component: () => import(/* webpackChunkName */ "@/views/Dashboard/GeneticAnalyst/Orders")
       },
       {
@@ -44,7 +57,8 @@ export default [
         name: "ga-order-details",
         meta: { pageHeader: "Details", parent: "ga-orders" },
         component: () => import(/* webpackChunkName */ "@/views/Dashboard/GeneticAnalyst/Orders/Details")
-      }
+      },
+      ...registrationRoutes
     ]
   }
 ]
