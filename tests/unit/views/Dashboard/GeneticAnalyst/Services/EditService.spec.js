fa6227a6df89072a26af12cdd871bae2d48332b6
Azmi Muhammad <azmim85@gmail.com>
2022-06-03 07:22:07 +0700

    test: unit testing edit service

---
+++ tests/unit/views/Dashboard/GeneticAnalyst/Services/EditService.spec.js
@@ -0,0 +1,57 @@
+import { createLocalVue, shallowMount, config } from "@vue/test-utils"
+import GAEditService from "@/views/Dashboard/GeneticAnalyst/Services/EditService"
+import Vuex from "vuex"
+
+describe("Genetic Analysis Edit Service", () => {
+  let container
+  let localVue = null
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+  })
+
+  afterEach(() => {
+    localVue = null
+  })
+
+  it("Should render", () => {
+    // GAEditService.created = {
+    //   getData: jest.fn("ID"),
+    //   getTxWeight: jest.fn()
+    // }
+    GAEditService.methods = {
+      cancelEdit: jest.fn(),
+      goToServicePage: jest.fn(),
+      onSubmitService: jest.fn({
+        currency: "",
+        description: "",
+        duration: "",
+        durationType: "",
+        name: "",
+        testResultSample: "",
+        totalPric: ""
+      })
+    };
+    container = shallowMount(GAEditService, {
+      localVue,
+      store: new Vuex.Store({
+        state: {
+          substrate: {
+            api: "API",
+            wallet: "WALLET",
+            mnemonicData: "oil spend nation obey lecture behave lake diary reward forest gym apple"
+          },
+          auth: {
+            loadingData: "LOADING"
+          },
+          metamask: {
+            web3: "WEB3"
+          }
+        }
+      })
+    })
+
+    expect(container.exists()).toBeTruthy()
+  })
+})
