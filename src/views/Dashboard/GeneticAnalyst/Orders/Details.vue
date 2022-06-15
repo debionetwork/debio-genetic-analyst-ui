5d4433df6280789cf0c310d13bbec18d1a9d84e8
Dicki Maulana <bukandicki@gmail.com>
2022-06-16 02:13:40 +0700

    fix: genetic analyst download file (#49)

--- src/views/Dashboard/GeneticAnalyst/Orders/Details.vue
+++ src/views/Dashboard/GeneticAnalyst/Orders/Details.vue
@@ -377,7 +377,21 @@ export default {
       immediate: true,
       handler: generalDebounce(async function(val) {
         if (val?.section === "geneticAnalysisOrders" || val?.section === "geneticAnalysis") await this.prepareData(this.$route.params.id)
-        if (val?.method === "GeneticAnalysisResultReady") this.step = 3
+        if (
+          val?.method === "GeneticAnalysisRejected" ||
+          val?.method === "GeneticAnalysisOrderRefunded"
+        ) {
+          this.isLoading = false
+          this.handleHideModalReject()
+          this.orderRejected = true
+        }
+        if (
+          val?.method === "GeneticAnalysisResultReady" ||
+          val?.method === "GeneticAnalysisOrderFulfilled"
+        ) {
+          this.isUploading = false
+          this.step = 3
+        }
       }, 100)
     },
 
