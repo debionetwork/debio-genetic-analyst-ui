4d0c5806ed016aecfe3a28a1f2e21976b28a83df
ciniiia <cinia@blocksphere.id>
2022-06-17 08:02:29 +0700

    feat: add switch network to mainet / testnet

--- src/common/lib/metamask/index.js
+++ src/common/lib/metamask/index.js
@@ -25,9 +25,12 @@ export async function connectToMetamask() {
       store.commit("metamask/SET_WALLET_ADDRESS", { currentAccount: currentAccount, accountList: accounts })
       handleAccountsChanged(accounts, null)
     })
+
+    
+    let network = await window.ethereum.request({ method: "eth_chainId"})
     window.ethereum.on("chainChanged", handleChainChanged)
 
-    return { currentAccount: currentAccount, accountList: accounts }
+    return { currentAccount: currentAccount, accountList: accounts, network }
   }
   catch (err) {
     if (err.code === 4001) {
@@ -35,6 +38,13 @@ export async function connectToMetamask() {
     }
     throw err
   }
+}
+
+export async function handleSwitchChain(id) {
+  await window.ethereum.request({
+    method: "wallet_switchEthereumChain",
+    params: [{ chainId: id }]
+  });
 }
 
 export async function startApp() {
