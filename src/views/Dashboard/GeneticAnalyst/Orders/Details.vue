d71e15987060dbac067918336d08fbfaafb6e5bc
Dicki <bukandicki@gmail.com>
2022-08-11 11:48:41 +0700

    fix: Metadata not found

--- src/views/Dashboard/GeneticAnalyst/Orders/Details.vue
+++ src/views/Dashboard/GeneticAnalyst/Orders/Details.vue
@@ -379,6 +379,14 @@ export default {
       if (val) this.initialDataKey()
     },
 
+    $route: {
+      deep: true,
+      immediate: true,
+      handler: async function (val) {
+        await this.prepareData(val.params.id)
+      }
+    },
+
     lastEventData: {
       deep: true,
       immediate: true,
@@ -471,14 +479,14 @@ export default {
           ...data,
           analysis_info: {
             ...analysisData,
-            fileName: analystReportDocument.rows[0].metadata.name,
-            fileSize: this.formatBytes(analystReportDocument.rows[0].metadata?.keyvalues?.fileSize || analystReportDocument.rows[0].size)
+            fileName: analystReportDocument.rows[0]?.metadata?.name || analysisData.geneticLink.split("/").pop(),
+            fileSize: this.formatBytes(analystReportDocument.rows[0]?.metadata?.keyvalues?.fileSize || analystReportDocument?.rows[0]?.size || 0)
           },
           document: {
             ...geneticData,
             reportLink: data.geneticLink,
-            fileName: geneticLinkName.rows[0].metadata.name,
-            fileSize: this.formatBytes(geneticLinkName.rows[0].metadata?.keyvalues?.fileSize || geneticLinkName.rows[0].size)
+            fileName: geneticLinkName.rows[0]?.metadata?.name || data.geneticLink.split("/").pop(),
+            fileSize: this.formatBytes(geneticLinkName.rows[0]?.metadata?.keyvalues?.fileSize || geneticLinkName?.rows[0]?.size || 0)
           },
           createdAt: new Date(+data.createdAt.replaceAll(",", "")).toLocaleString("en-GB", {
             day: "numeric",
